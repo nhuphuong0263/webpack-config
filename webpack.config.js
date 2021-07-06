9a7a943276e37b02e07dbde76b5ee6e079183e43
Macintosh HD <macintoshhd@Macintoshs-MacBook-Pro.local>
2021-07-06 14:26:23 +0700

    add css, sass, postcss, hotreloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
-let mode = "development"
+let mode = "development";
+let target = "web";
 
 if (process.env.NODE_ENV === "production") {
-  mode = "production"
+  mode = "production",
+    target = "browserslist"
 }
 
 module.exports = {
   mode: mode,
+  target: target,
 
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        test: /\.(s[ac]|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
       },
       {
         test: /\.js$/,
@@ -25,7 +28,8 @@ module.exports = {
     ]
   },
 
-  plugins: [new MiniCssExtractPlugin],
+  plugins: [new MiniCssExtractPlugin()],
+
   devtool: "source-map",
   devServer: {
     contentBase: "./dist",
