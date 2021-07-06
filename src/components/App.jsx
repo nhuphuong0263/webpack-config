eb36d697aaf26e58d7a32cab956433e6fc627b46
Macintosh HD <macintoshhd@Macintoshs-MacBook-Pro.local>
2021-07-06 15:59:03 +0700

    images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "../styles/index.scss"
 import Recipes from './Recipes'
+import sword from '../images/swc-sword.png'
+import swordSvg from '../images/sword.svg'
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
         <section>
           <h1>Oh Hai, React</h1>
         </section>
+        <img src={sword} alt="sword" width="250" />
+        <img src={swordSvg} alt="sword" width="250" />
       </main>
 
       <Recipes />
